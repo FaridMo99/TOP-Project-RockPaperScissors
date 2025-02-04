f468f474a8a98525242b1ef88176301cfa590d95
FaridMo99 <farid-ahmad@hotmail.de>
2025-02-04 12:34:39 +0100

    removed js code/started brand new

--- javascript.js
+++ javascript.js
@@ -1,113 +1,92 @@
-// Computer choice algorithm.
+/*
+Step 2: Write the logic to get the computer choice
+Your game will be played against the computer. You will write a function that randomly returns “rock”, “paper” or “scissors”.
 
-function getRandomIntInclusive(min, max) {
-    const minCeiled = Math.ceil(min = 1);
-    const maxFloored = Math.floor(max = 3);
-    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); 
-  }
- 
-function getComputerChoice() {
-    if (getRandomIntInclusive() <= 1) {
-        return("Rock");
-    }
-
-    else if (getRandomIntInclusive() == 2) {
-        return("Paper");
-    }
-
-    else {
-        return("Scissors");
-    }
-}
-
-console.log(getComputerChoice());
-
-//User input Algorithm
-let choice = prompt("Choose between Rock Paper and Scissors");
-let answer = choice.toUpperCase();
-
-function getHumanChoice() {
-if (answer = "ROCK") {
-    return 1;
-}
-
-else if (answer = "PAPER") {
-    return 2;
-}
-else if (answer = "SCISSORS") {
-    return 3;
-}
-
-else {
-    return "Invalid answer!";
-}
-}
-
-// score
-
-let humanScore = 0;
-let computerScore = 0;
+Create a new function named getComputerChoice.
+Write the code so that getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”.
+Hint: The Math.random method returns a random number that’s greater than or equal to 0 and less than 1.
+ Think about how you can use this to conditionally return one of the multiple choices.
+Note: Some learners might think that arrays are necessary for this problem, but they aren’t.
+ If you’re unfamiliar with arrays, don’t worry - they’ll be covered later in the curriculum.
+Test that your function returns what you expect using console.log or the browser developer tools before advancing to the next step.
+*/
 
 
-//algorithm playing rounds
-
-function playRound (humanChoice, computerChoice) {
-    humanChoice = getHumanChoice;
-    computerChoice = getComputerChoice;
-
-    if (humanChoice == 1 && computerChoice == "Paper") {
-        console.log("You Lost, Paper beats Rock");
-        computerScore += 1;
-    }
-
-    else if (humanChoice == 1 && computerChoice == "Scissors") {
-        console.log("You Won, Rock beats Scissors");
-        humanScore += 1;
-    }
-
-    else if (humanChoice ==  2 && computerChoice == "Rock" ) {
-        console.log("You Won, Paper beats Rock");
-        humanScore += 1;
-    }
-
-    else if (humanChoice ==  2 && computerChoice == "Scissors" ) {
-        console.log("You Lost, Scissors beats Paper");
-        computerScore += 1;
-    }
-
-    else if (humanChoice ==  3 && computerChoice == "Rock") {
-        console.log("You Lost, Rock beats Scissors");
-        computerScore += 1;
-    }
-
-    else if (humanChoice ==  3 && computerChoice == "Paper") {
-        console.log("You Won, Scissors beats Paper");
-        humanScore += 1;
-    }
-
-    else {
-        console.log("Draw");
-
-    }
-}
-// algorithm full game
-
-function playGame() {
-
-}
 
 
-/*  
+
+
+
+
+/*
+Step 3: Write the logic to get the human choice
+Your game will be played by a human player. You will write a function that takes the user choice and returns it.
+
+Create a new function named getHumanChoice.
+Write the code so that getHumanChoice will return one of the valid choices depending on what the user inputs.
+Hint: Use the prompt method to get the user’s input.
+Test what your function returns by using console.log.
+*/
+
+
+
+
+
+
+
+
+
+/*
+Step 4: Declare the players score variables
+Your game will keep track of the players score. You will write variables to keep track of the players score.
+
+Create two new variables named humanScore and computerScore in the global scope.
+Initialize those variables with the value of 0.
+*/
+
+
+
+
+
+
+
+
+
+
+
+/*
+Step 5: Write the logic to play a single round
+Your game will be played round by round. You will write a function that takes the human and computer player choices as arguments,
+ plays a single round, increments the round winner’s score and logs a winner announcement.
+
+Create a new function named playRound.
+Define two parameters for playRound: humanChoice and computerChoice. Use these two parameters to take the human and computer choices
+ as arguments.
+Make your function’s humanChoice parameter case-insensitive so that players can input “rock”, “ROCK”, “RocK”, or other variations.
+Write the code for your playRound function to console.log a string value representing the round winner, such as:
+ “You lose! Paper beats Rock”.
+Increment the humanScore or computerScore variable based on the round winner.
+*/
+
+
+
+
+
+
+
+
+/*
 Step 6: Write the logic to play the entire game
-Your game will play 5 rounds. You will write a function named playGame that calls playRound to play 5 rounds, keeps track of the scores and declares a winner at the end.
+Your game will play 5 rounds. You will write a function named playGame that calls playRound to play 5 rounds, keeps track of the 
+scores and declares a winner at the end.
 
 Create a new function named playGame.
 Move your playRound function and score variables so that they’re declared inside of the new playGame function
 Play 5 rounds by calling playRound 5 times.
-Hint: When you assign a function call to a variable, the return value of that function is assigned to the variable. 
-Accessing the variable afterward will only provide the assigned value; it doesn’t recall the function. 
-You need to recall the choice functions to get new choices for each round.
-Re-work your previous functions or create more helper functions if necessary. Specifically, you may want to change the 
-return values to something more useful.
-If you already know about loops, you can use them. If not, don’t worry! Loops will be covered in the next lesson.  
+Hint: When you assign a function call to a variable, the return value of that function is assigned to the variable.
+ Accessing the variable afterward will only provide the assigned value; it doesn’t recall the function. You need to recall the
+  choice functions to get new choices for each round.
+Re-work your previous functions or create more helper functions if necessary. Specifically, you may want to change the return
+ values to something more useful.
+If you already know about loops, you can use them. If not, don’t worry! Loops will be covered in the next lesson.
 */

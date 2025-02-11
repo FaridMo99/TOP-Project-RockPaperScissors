c776d93e28d21320fe519173b6f7afe627e16ec4
FaridMo99 <farid-ahmad@hotmail.de>
2025-02-11 22:21:31 +0100

    finished connecting UI to game

--- javascript.js
+++ javascript.js
@@ -2,6 +2,11 @@
 let div = document.createElement("div")
 div.setAttribute("id", "results")
 
+let divLeft = document.createElement("div")
+let divRight = document.createElement("div")
+
+div.appendChild(divLeft)
+div.appendChild(divRight)
 //algorithm for computer choice
 
  //function for randomizer between 1-9
@@ -46,68 +51,116 @@ let humanScore = 0;
 let computerScore = 0;
 
 
-// styles for result div
-results.style.cssText = "width: 87.5%; height: 25%; background-color: var(--div-bg-color); align-self: flex-end; display: flex; flex-direction: column; position:absolute; justify-content: space-around; align-items: center; border: 1px solid purple; box-shadow: 0 0 50px purple; color: black; font-weight: bold; font-size: 1.3em; font-family: Arial; bottom: 5vh;"
-results.style.visibility = "hidden";
+// styles for javascript divs 
+div.style.cssText = "width: 87.5%; height: 25%; background-color: var(--div-bg-color); align-self: flex-end; display: flex; position:absolute; justify-content: space-between; border: 1px solid purple; box-shadow: 0 0 50px purple; color: white; font-weight: bold; font-size: 1.3em; font-family: Arial; bottom: 5vh;"
+div.style.visibility = "hidden";
+
+divLeft.style.cssText = "width: 80%; height: 100%; display: flex; flex-direction: column; justify-content: space-around; align-items: center;"
+divRight.style.cssText = "width: 20%; height: 100%; display: flex; flex-direction: column; margin-top: 5%; margin-right: 5vw;"
+
 // Function to handle human choice
 function getChoice(humanChoice) {
-    results.style.visibility = "visible";
+    div.style.visibility = "visible";
     let computerChoice = getComputerChoice(); 
     playRound(humanChoice, computerChoice);
 }
 
+
+
+
+let rounds = 0
+
+divRight.innerHTML = `<span>Score: ${humanScore}</span><span>Enemy Score: ${computerScore}</span><span>Round: ${rounds}</span>`
+
+
+function playGame() {
+rounds++
+divRight.innerHTML = `<span>Score: ${humanScore}</span><span>Enemy Score: ${computerScore}</span><span>Round: ${rounds}</span>`
+if (rounds % 5 == 0 && humanScore > computerScore) {
+    divLeft.innerHTML += "<span>You Won!</span>"
+    rounds = 0
+    humanScore = 0
+    computerScore = 0
+}
+
+else if (rounds % 5 == 0 && humanScore < computerScore){
+    divLeft.innerHTML += "<span>You Lost!</span>"
+    rounds = 0
+    humanScore = 0
+    computerScore = 0
+}
+
+else if (rounds % 5 == 0 && humanScore == computerScore) {
+    divLeft.innerHTML += "<span>Game ended in a Draw!</span>"
+    rounds = 0
+    humanScore = 0
+    computerScore = 0
+}
+}
+
+
+
+
+
+
 //algorithm for players score
 
 function playRound(humanChoice, computerChoice) {
-    results.innerHTML = `<span>You chose ${humanChoice} computer chose ${computerChoice}</span>`
- if (humanChoice == "Rock" && computerChoice == "Paper") {
-    results.innerHTML += "You lost Rock loses to Paper";
-    computerScore++;
-    return 1; 
+
+    divLeft.innerHTML = `<span>You chose ${humanChoice}</span> <span>Computer chose ${computerChoice}</span>`
+    if (humanChoice == "Rock" && computerChoice == "Paper") {
+    divLeft.innerHTML += "You lost Rock loses to Paper";
+    divRight.innerHTML = `<span>Score: ${humanScore}</span><span>Enemy Score: ${computerScore}</span><span>Round: ${rounds}</span>`
+    computerScore++; 
+  
  }
 
  else if (humanChoice == "Rock" && computerChoice == "Scissors") {
-    results.innerHTMl += "You Won Rock beats Scissors";
+    divLeft.innerHTML += "You Won Rock beats Scissors";
+    divRight.innerHTML = `<span>Score: ${humanScore}</span><span>Enemy Score: ${computerScore}</span><span>Round: ${rounds}</span>`
     humanScore++;
-    return 1;
+   
  }
 
  else if (humanChoice == "Paper" && computerChoice == "Rock") {
-    results.innerHTML += "You Won Paper beats Rock";
+    divLeft.innerHTML += "You Won Paper beats Rock";
+    divRight.innerHTML = `<span>Score: ${humanScore}</span><span>Enemy Score: ${computerScore}</span><span>Round: ${rounds}</span>`
     humanScore++;
-    return 1;  
+    
  }
 
  else if (humanChoice == "Paper" && computerChoice == "Scissors") {
-    results.innerHTML += "You lost Paper loses to Scissors";
+    divLeft.innerHTML += "You lost Paper loses to Scissors";
+    divRight.innerHTML = `<span>Score: ${humanScore}</span><span>Enemy Score: ${computerScore}</span><span>Round: ${rounds}</span>`
     computerScore++;
-    return 1;   
+    
  }
 
  else if (humanChoice == "Scissors" && computerChoice == "Rock") {
-    results.innerHTML += "You lost Scissors loses to Rock";
+    divLeft.innerHTML += "You lost Scissors loses to Rock";
+    divRight.innerHTML = `<span>Score: ${humanScore}</span><span>Enemy Score: ${computerScore}</span><span>Round: ${rounds}</span>`
     computerScore++;
-    return 1;   
+     
  }
 
  else if (humanChoice == "Scissors" && computerChoice == "Paper") {
-    results.innerHTML += "You Won Scissors beats Paper";
+    divLeft.innerHTML += "You Won Scissors beats Paper";
+    divRight.innerHTML = `<span>Score: ${humanScore}</span><span>Enemy Score: ${computerScore}</span><span>Round: ${rounds}</span>`
     humanScore++;
-    return 1;  
+    
  }
 
  else {
-    results.innerHTML += "Draw!";
-    return 1;
+    divLeft.innerHTML += "Draw!";
+    
  }
+playGame();
 
+return 1
 }
 
 
-//algorithm for adding Rounds til 5 and ending the game
 
-function playGame() {
- 
-}
+
 
 
